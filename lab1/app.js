d0d6e31550ba37c0e81d78d02cd2eef8dbef63e8
Hiếu Code <hieutran114022000@gmail.com>
2023-03-09 23:28:43 +0700

    code

---
+++ lab1/app.js
@@ -0,0 +1,11 @@
+// thông báo mời nhâp a ,b
+let a = prompt("Mời bạn nhập số nguyên a");
+let b = prompt("mời bạn nhập số nguyên b");
+// chuyển kiểu dữ liệu string sang kiểu int
+a=parseInt(a,10);
+b=parseInt(b,10);
+// tạo biến c gán = giá trị a + b
+var c=a+b;
+// in kết quả
+document.write("Tổng = " + c);
+console.log(c);
